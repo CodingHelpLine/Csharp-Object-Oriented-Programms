f88c7e5d09ca7c3fa0ed59a3eca53d7dbe13d0f6
CodingHelpLine <74076982+CodingHelpLine@users.noreply.github.com>
2022-09-24 04:58:12 +0500

    Upload Salary Calculator Window Form app

---
+++ EmployeeSalaryWindowFormApp/EmployeeSalaryGUI/EmployeeSalaryGUI/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace EmployeeSalaryGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMain = new System.Windows.Forms.Label();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.lblRate = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblHourlyRate = new System.Windows.Forms.Label();
+            this.lblHoursWorked = new System.Windows.Forms.Label();
+            this.lblOvertimePay = new System.Windows.Forms.Label();
+            this.lblRegularPay = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTotalPay = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMain
+            // 
+            this.lblMain.AutoSize = true;
+            this.lblMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMain.Location = new System.Drawing.Point(66, 24);
+            this.lblMain.Name = "lblMain";
+            this.lblMain.Size = new System.Drawing.Size(331, 31);
+            this.lblMain.TabIndex = 0;
+            this.lblMain.Text = "Hourly Salary Calculator";
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.Location = new System.Drawing.Point(35, 96);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(119, 20);
+            this.lblHours.TabIndex = 1;
+            this.lblHours.Text = "Hours Worked: ";
+            // 
+            // txtHours
+            // 
+            this.txtHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHours.Location = new System.Drawing.Point(160, 93);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(152, 26);
+            this.txtHours.TabIndex = 2;
+            // 
+            // txtRate
+            // 
+            this.txtRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRate.Location = new System.Drawing.Point(160, 128);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(152, 26);
+            this.txtRate.TabIndex = 4;
+            // 
+            // lblRate
+            // 
+            this.lblRate.AutoSize = true;
+            this.lblRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRate.Location = new System.Drawing.Point(35, 131);
+            this.lblRate.Name = "lblRate";
+            this.lblRate.Size = new System.Drawing.Size(101, 20);
+            this.lblRate.TabIndex = 3;
+            this.lblRate.Text = "Hourly Rate: ";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(336, 93);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(111, 58);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Hourly Rate: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(68, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Hours Worked: ";
+            // 
+            // lblHourlyRate
+            // 
+            this.lblHourlyRate.AutoSize = true;
+            this.lblHourlyRate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblHourlyRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHourlyRate.Location = new System.Drawing.Point(216, 232);
+            this.lblHourlyRate.MaximumSize = new System.Drawing.Size(150, 30);
+            this.lblHourlyRate.MinimumSize = new System.Drawing.Size(150, 30);
+            this.lblHourlyRate.Name = "lblHourlyRate";
+            this.lblHourlyRate.Size = new System.Drawing.Size(150, 30);
+            this.lblHourlyRate.TabIndex = 9;
+            this.lblHourlyRate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHoursWorked
+            // 
+            this.lblHoursWorked.AutoSize = true;
+            this.lblHoursWorked.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblHoursWorked.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoursWorked.Location = new System.Drawing.Point(216, 197);
+            this.lblHoursWorked.MaximumSize = new System.Drawing.Size(150, 30);
+            this.lblHoursWorked.MinimumSize = new System.Drawing.Size(150, 30);
+            this.lblHoursWorked.Name = "lblHoursWorked";
+            this.lblHoursWorked.Size = new System.Drawing.Size(150, 30);
+            this.lblHoursWorked.TabIndex = 8;
+            this.lblHoursWorked.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOvertimePay
+            // 
+            this.lblOvertimePay.AutoSize = true;
+            this.lblOvertimePay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOvertimePay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOvertimePay.Location = new System.Drawing.Point(216, 312);
+            this.lblOvertimePay.MaximumSize = new System.Drawing.Size(150, 30);
+            this.lblOvertimePay.MinimumSize = new System.Drawing.Size(150, 30);
+            this.lblOvertimePay.Name = "lblOvertimePay";
+            this.lblOvertimePay.Size = new System.Drawing.Size(150, 30);
+            this.lblOvertimePay.TabIndex = 13;
+            this.lblOvertimePay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRegularPay
+            // 
+            this.lblRegularPay.AutoSize = true;
+            this.lblRegularPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblRegularPay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegularPay.Location = new System.Drawing.Point(216, 272);
+            this.lblRegularPay.MaximumSize = new System.Drawing.Size(150, 30);
+            this.lblRegularPay.MinimumSize = new System.Drawing.Size(150, 30);
+            this.lblRegularPay.Name = "lblRegularPay";
+            this.lblRegularPay.Size = new System.Drawing.Size(150, 30);
+            this.lblRegularPay.TabIndex = 12;
+            this.lblRegularPay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(68, 312);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Overtime Pay: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(68, 272);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Regular Pay: ";
+            // 
+            // lblTotalPay
+            // 
+            this.lblTotalPay.AutoSize = true;
+            this.lblTotalPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTotalPay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalPay.Location = new System.Drawing.Point(216, 356);
+            this.lblTotalPay.MaximumSize = new System.Drawing.Size(150, 30);
+            this.lblTotalPay.MinimumSize = new System.Drawing.Size(150, 30);
+            this.lblTotalPay.Name = "lblTotalPay";
+            this.lblTotalPay.Size = new System.Drawing.Size(150, 30);
+            this.lblTotalPay.TabIndex = 15;
+            this.lblTotalPay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(68, 356);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(74, 20);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Total Pay";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 399);
+            this.Controls.Add(this.lblTotalPay);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblOvertimePay);
+            this.Controls.Add(this.lblRegularPay);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblHourlyRate);
+            this.Controls.Add(this.lblHoursWorked);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.lblRate);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.lblMain);
+            this.Name = "Form1";
+            this.Text = "Employee Salary Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMain;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Label lblRate;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblHourlyRate;
+        private System.Windows.Forms.Label lblHoursWorked;
+        private System.Windows.Forms.Label lblOvertimePay;
+        private System.Windows.Forms.Label lblRegularPay;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblTotalPay;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ EmployeeSalaryWindowFormApp/EmployeeSalaryGUI/EmployeeSalaryGUI/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeSalaryGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Button event handler
+        /// 
+        ///     1. Parse Rate & hours
+        ///     2. Calculate overtime
+        ///     3. Calculate Regular Pay
+        ///     4. Total 
+        ///     5. Display
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double hourlyRate = 0;          // hourly rate
+            int hours = 0;                  // hours worked
+            double overtimePay = 0;         // overtime pay
+            double regularPay = 0;          // regular pay
+            double totalPay = 0;            // total pay
+            int tempHours = 0;              // temp hours storage
+
+            // Parse values
+            if(int.TryParse(txtHours.Text, out hours))
+            {
+                // Parse hourly rate
+                if (double.TryParse(txtRate.Text, out hourlyRate))
+                {
+                    tempHours = hours;
+
+                    // Overtime Salary
+                    if (tempHours > 40)
+                    {
+                        overtimePay = (1.5 * hourlyRate) * (tempHours - 40);
+                        tempHours = 40; 
+                    }
+
+                    // regular pay
+                    regularPay = tempHours * hourlyRate;
+
+                    // total pay
+                    totalPay = overtimePay + regularPay;
+
+                    // now display
+                    lblHoursWorked.Text = hours.ToString();
+                    lblHourlyRate.Text = hourlyRate.ToString("C02");
+                    lblRegularPay.Text = regularPay.ToString("C02");
+                    lblOvertimePay.Text = overtimePay.ToString("C02");
+                    lblTotalPay.Text = totalPay.ToString("C02");
+
+                    // clear inputs
+                    txtHours.Text = "";
+                    txtRate.Text = "";
+
+                    txtHours.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid hourly rate entered");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid hours entered");
+            }
+        }
+    }
+}
